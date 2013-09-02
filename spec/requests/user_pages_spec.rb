9ee435a68c9d41190cb624058866a9a0a0d7192d
nelson <mh732@ndeveloper.com>
2013-09-02 09:40:11 -0500

    finished first signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  subject{ page}
+  subject { page }
 
   describe "sing up page" do
-    before { visit signup_path}
-    it {should have_content ('SignUp')}
-    it {should have_title(full_title('Signup'))}
+    before { visit signup_path }
+    it { should have_content ('Sign Up') }
+    it { should have_title(full_title('Sign Up')) }
   end
-
 end
